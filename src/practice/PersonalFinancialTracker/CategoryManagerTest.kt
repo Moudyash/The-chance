ac93dfae0f2ce240c7488744f359b7049d675ee9
Moudyash <medo.ash.2001@gmail.com>
2025-04-09 00:02:03 +0300

    update testcases

--- src/practice/PersonalFinancialTracker/CategoryManagerTest.kt
+++ src/practice/PersonalFinancialTracker/CategoryManagerTest.kt
@@ -13,11 +13,15 @@ fun CheckCategory(name: String, result: Any, expected: Any) {
 
     fun runCategoryManagerTests() {
         val manager = CategoryManager()
-
+        val longName = "b".repeat(50)
+        CheckCategory("Add new category with more than max length (50 characters)", manager.addCategory(longName), true)
+        CheckCategory("Add new category with less than min length (3 characters)", manager.addCategory("te"), true)
         CheckCategory("Add new category: Gaming", manager.addCategory("Gaming"), true)
         CheckCategory("Add duplicate category: gaming", manager.addCategory("GAMING"), true)
+        CheckCategory("Add empty category name", manager.addCategory(""), true)
         CheckCategory("Update category: Gaming to eSports", manager.updateCategory("gaming", "eSports"), true)
         CheckCategory("Update non existing  category: Tea to eSports", manager.updateCategory("tea", "eSports"), true)
+        CheckCategory("Prevent update to empty name", manager.updateCategory("eSports", ""), true)
         CheckCategory("Delete category: eSports", manager.deleteCategory("Esports"), true)
         CheckCategory("Delete non existing category: Coffee", manager.deleteCategory("Coffee"), true)
         CheckCategory("Check non existing category: Coffee", manager.categoryExists(""), true)
